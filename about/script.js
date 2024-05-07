4d65448d013525d24b1e8948089858a63221a660
KagariET01 <KagariET01@users.noreply.github.com>
2024-05-07 14:21:08 +0800

    one line 化

--- about/script.js
+++ about/script.js
@@ -1,7 +1,1 @@
-
-function show(a){
-	if(document.getElementById(a).classList.contains("hide"))
-		document.getElementById(a).classList.remove("hide");
-	else
-		document.getElementById(a).classList.add("hide");
-}
+function show(a){if(document.getElementById(a).classList.contains("hide")){document.getElementById(a).classList.remove("hide");}else{document.getElementById(a).classList.add("hide");}}
